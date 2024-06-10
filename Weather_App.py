19a5b0b79c63489a415ac636217d0257cca87e8e
jitesh sharma <you@example.com>
2024-06-10 22:52:19 +0530

    weather_app_speaking

---
+++ Weather_App.py
@@ -0,0 +1,22 @@
+from gtts import gTTS
+from playsound import playsound
+import requests
+import json
+
+import os
+
+city=input("Enter a City Name by user : \n")
+
+url= f"http://api.weatherapi.com/v1/current.json?key=91dbec122cfd492b88a172049242105&q={city}&aqi=yes"
+
+r=requests.get(url)
+print(r.text)
+wdic=json.loads(r.text)
+g=wdic["current"]["temp_c"]
+h="Temperature of "+city +"at current position is "+ str(g) 
+print(city)
+print(g)
+fh=gTTS(h,lang='en')
+fh.save('weather.mp3')
+playsound('weather.mp3')
+os.remove('weather.mp3')
